8a43120daeb8696173f06f96ae35b032bc3d96fa
keerthidas <keerthidas@users.noreply.github.com>
2020-11-16 13:14:06 +0530

    reloading issues checking

--- application/modules/supplier/supplier_dashboard/views/administration.php
+++ application/modules/supplier/supplier_dashboard/views/administration.php
@@ -5,10 +5,10 @@
 			<div id="tg-twocolumns" class="tg-twocolumns tg-main-section tg-haslayout">
 				<div class="col-md-9 col-sm-8 col-xs-12 pull-right">
 
-                <div id='panels_frame' class="panels">
-                    <div class='panel'><iframe  src=""></iframe></div>
+                <!-- <div id='panels_frame' class="panels"> -->
+                    <iframe id="iframe_src" src="" style="overflow:hidden;height:-webkit-fill-available;width:100%" width="100%"></iframe>
 
-                </div>
+                <!-- </div> -->
 
 				</div>
 				<div class="col-md-3 col-sm-4 col-xs-12">
@@ -18,3 +18,13 @@
 		</div>
 	</div>
 </main>
+
+<script>
+    // Selecting the iframe element
+    var iframe = document.getElementById("myIframe");
+    
+    // Adjusting the iframe height onload event
+    iframe.onload = function(){
+        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+    }
+    </script>
